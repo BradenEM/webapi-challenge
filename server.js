cb95425b86158d9e9d4ac3b92bce7efd61138376
Braden Mason <bradenemason@gmail.com>
2019-08-03 12:02:46 -0500

    all project endpints are working

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const projectsRouter = require("./data/projects/projectsRouter");
 const server = express();
 
 server.use(obscurity());
+server.use(logger);
 server.use(express.json());
-// server.use("/api/projects", projectsRouter);
+server.use("/api/projects", projectsRouter);
 // server.use("/api/actions", actionsRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Sprint Time</h1>");
 });
+
+function logger(req, res, next) {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const timestamp = `${hours}:${minutes}:${seconds}`;
+  console.log(`${req.method} on path: "${req.originalUrl}" at ${timestamp}`);
+  next();
+}
 
 module.exports = server;
